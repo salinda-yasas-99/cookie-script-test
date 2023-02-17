574b69399ab39a79029f3fda0aee1ad0d2ef4ac2
salinda-yasas-99 <salindasym@gamil.com>
2023-02-17 13:52:56 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Salinda yasas</p>
+        <p>Amitha Samarasinghe</p>
         <a
           className="App-link"
           href="https://reactjs.org"
