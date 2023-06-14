46c00f1c074bc7de2f1a1902ed0566198d535aae
lakshankd <dominiclakshank@gmail.com>
2023-06-14 06:43:20 +0530

    adding default cookies.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo.jpeg";
+import "./App.css";
+import Cookies from "js-cookie";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    handleCreateCookie();
+  }, []);
+
+  const handleCreateCookie = () => {
+    // Set a cookie with name 'myCookie' and value 'example-value'
+    Cookies.set("myCookie1", "example-value");
+
+    // Set a cookie with name 'myCookie' and value 'example-value' that expires in 7 days
+    Cookies.set("myCookie2", "example-value", { expires: 7 });
+
+    // Set a cookie with name 'myCookie' and value 'example-value' that expires in 1 hour
+    Cookies.set("myCookie3", "example-value", {
+      expires: new Date(Date.now() + 3600000),
+    });
+
+    // Set a cookie with name 'myCookie' and value 'example-value' that is secure (HTTPS only)
+    Cookies.set("myCookie4", "example-value", { secure: true });
+
+    // Set a cookie with name 'myCookie' and value 'example-value' with additional options
+    Cookies.set("myCookie5", "example-value", {
+      expires: 7,
+      path: "/",
+      domain: "example.com",
+      secure: true,
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Salinda yasas</p>
-        <p>Amitha Samarasinghe</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 style={{ marginTop: "100px" }}>WebSafe</h3>
+        <p>Open Source Cookie Consent Management System</p>
+        <p>Test Website</p>
       </header>
     </div>
   );
